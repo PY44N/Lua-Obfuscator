4aaaf80cea36e661118976d755976c0c3d5eb5e0
Realityyxd <110492268+Realityyxd@users.noreply.github.com>
2022-08-17 15:36:03 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,133 @@
-print("Lua Obfuscator by Pyan")
+print("if game.PlaceId == 4282985734 then 
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
+
+local win = Flux:Window("Realityy Hub", game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+local tab = win:Tab("Main", "http://www.roblox.com/asset/?id=6023426915")
+tab:Button("Unamed Script", "No info.", function()
+Flux:Notification("Loaded!", "Alright")
+loadstring(game:HttpGet"https://pastebin.com/raw/4iNcg1cT")()
+end)
+tab:Button("Project Hook", "No Info", function()
+Flux:Notification("Loaded!", "Alright")
+loadstring(game:HttpGet("https://projecthook.xyz/scripts/free.lua"))()
+end)
+tab:Line()
+
+
+end
+
+
+
+if game.PlaceId == 648362523 then
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
+
+local win = Flux:Window("Realityy Hub", game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+local tab = win:Tab("Main", "http://www.roblox.com/asset/?id=6023426915")
+tab:Button("INF Credits Autofarm", "No info.", function()
+Flux:Notification("Loaded!", "Alright")
+getgenv().AutoFarm = true
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/littlegui/main/Breaking-Point'))()
+end)
+tab:Button("Outlaws Hub", "No Info", function()
+Flux:Notification("Loaded!", "Alright")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MostafaXc00dy/MostafaXc00dy/main/OutlawsHub/Free/V6.lua"))()
+
+end)
+tab:Button("Funny Script", "Credits: Squidfan420", function()
+Flux:Notification("Loaded!", "Alright")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ColdStep2/Breaking-Point-Funny-Squid-Hax/main/Breaking%20Point%20Funny%20Squid%20Hax",true))();
+end)
+tab:Line()
+
+
+end
+
+
+
+if game.PlaceId == 292439477 then
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
+
+local win = Flux:Window("Realityy Hub", game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+local tab = win:Tab("Main", "http://www.roblox.com/asset/?id=6023426915")
+tab:Button("HyperX", "No info.", function()
+Flux:Notification("Loaded!", "Alright")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mick-gordon/fsfhiwhwio-pgweijogwejpwgekjowgkjo/main/README.md", true))()
+end)
+tab:Button("Solaris Hub", "No Info", function()
+Flux:Notification("Loaded!", "Alright")
+loadstring(game:HttpGet('https://solarishub.net/script.lua',true))()
+end)
+tab:Button("Sirius Loader", "No Info", function()
+Flux:Notification("Loaded!", "Alright")
+loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Sirius/request/Loader'))()
+end)
+tab:Line()
+
+
+end
+
+
+if game.PlaceId == 2753915549 then
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
+
+local win = Flux:Window("Realityy Hub", game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+local tab = win:Tab("Main", "http://www.roblox.com/asset/?id=6023426915")
+tab:Button("HoHo Hub", "No info.", function()
+Flux:Notification("Loaded!", "Alright")
+loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
+end)
+tab:Button("Vellerius", "Join their discord for key", function()
+Flux:Notification("Loaded!", "Alright")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Corrupt2625/pjv/main/Bloxfruit.lua"))()
+end)
+tab:Button("Hxllow Hub", "No Info", function()
+Flux:Notification("Loaded!", "Alright")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/master/hxllow"))()
+end)
+tab:Line()
+
+
+end
+
+if game.PlaceId == 142823291 then
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
+
+local win = Flux:Window("Realityy Hub", game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+local tab = win:Tab("Main", "http://www.roblox.com/asset/?id=6023426915")
+tab:Button("Eclipse Hub", "No info.", function()
+Flux:Notification("Loaded!", "Alright")
+local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth"c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
+
+end)
+tab:Button("V.G Hub", "No info", function()
+Flux:Notification("Loaded!", "Alright")
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()end)
+tab:Button("Xenny Ware", "No Info", function()
+Flux:Notification("Loaded!", "Alright")
+repeat wait() until game.Players.LocalPlayer.Character
+url = "https://raw.githubusercontent.com/xennyy/Xenny-Ware/main/loader.lua"
+loadstring(game:HttpGet(url))()
+end)
+tab:Line()
+
+
+end
+
+
+if game.PlaceId == 6152116144 then
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
+
+local win = Flux:Window("Realityy Hub", game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+local tab = win:Tab("Main", "http://www.roblox.com/asset/?id=6023426915")
+tab:Button("LeanSlayers", "No info.", function()
+Flux:Notification("Loaded!", "Alright")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KeoneGithub/KeoneGithub/main/LeanSlayer"))()
+
+end)
+tab:Button("Ewyzu White Loader", "No info", function()
+Flux:Notification("Loaded!", "Alright")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ewyzu/ewyzu-scripts/main/Project-Slayers"))()tab:Line()
+end)
+
+end
+")
